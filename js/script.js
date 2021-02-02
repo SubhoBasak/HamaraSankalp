f59bc306dde9f5b2eb851c269583b599815b2246
SubhoBasak <subhobasak50@gmail.com>
2021-02-02 13:00:02 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// =================== back to top button =======================
+//Get the button:
+var b2top = document.getElementById("b2top");
+console.log(b2top);
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    b2top.style.display = "block";
+  } else {
+    b2top.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
